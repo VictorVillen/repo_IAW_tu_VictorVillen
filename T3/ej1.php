20576ee78fb92ebb5cc2254a6440bce3cff5979d
Victor Villen <vicvilveg@alu.edu.gva.es>
2024-12-18 13:19:16 +0100

    he subido el ejercicio 1

---
+++ T3/ej1.php
@@ -0,0 +1,13 @@
+<?php
+//Numero aleatorio del 1 al 3
+$var1 = rand(1, 3);
+//Mostrar el numero generado
+print "<p>El numero generado es: $var1"
+//Condicional para que te diga en letra el numero que ha salido
+if ($var1 == 1) {
+    print "<p>Numero en letra: uno</p>"
+}   elseif ($var1 == 2) {
+    print "<p>Numero en letra: dos</p>"
+}   else{
+    print "<p>Numero en letra: tres</p>"
+}
